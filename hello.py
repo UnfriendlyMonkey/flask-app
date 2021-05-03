30ea27a1fb9f447f2139211632e4f866082c8475
Andrey Sereda <andreysereda@ya.ru>
2021-05-03 12:01:02 +0300

    yet another try

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Я заебался!'
+    return 'Я заебался совсем!'
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get(self):
         r = self.app.get('/')
-        self.assertEqual(r.data.decode('utf-8'), 'Я заебался!')
+        self.assertEqual(r.data.decode('utf-8'), 'Я заебался совсем!')
 
     def test_post(self):
         r = self.app.post('/')
